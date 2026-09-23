eca84e9bbbdd77371ae1fecb973f391cde72e02d
Mayo <signup+github@mayonissen.com>
2026-09-22 20:40:02 -0400

    transparent fill

--- public/app.js
+++ public/app.js
@@ -581,10 +581,14 @@ async function generatePDF() {
         const h = (duration / 60) * hourH - 0.016;
         const closed = isClosed(ev.name);
 
-        doc.setFillColor(255, 255, 255);
         doc.setDrawColor(closed ? 255 : GREEN_DARK[0], closed ? 255 : GREEN_DARK[1], closed ? 255 : GREEN_DARK[2]);
         doc.setLineWidth(closed ? 0.003 : 0.014);
-        doc.roundedRect(x, y, w, h, 0.02, 0.02, 'FD');
+        if (closed) {
+          doc.roundedRect(x, y, w, h, 0.02, 0.02, 'D');
+        } else {
+          doc.setFillColor(255, 255, 255);
+          doc.roundedRect(x, y, w, h, 0.02, 0.02, 'FD');
+        }
 
         doc.setTextColor(closed ? CLOSED_TEXT[0] : GREEN_DARK[0], closed ? CLOSED_TEXT[1] : GREEN_DARK[1], closed ? CLOSED_TEXT[2] : GREEN_DARK[2]);
         doc.setFont('helvetica', 'bold');
